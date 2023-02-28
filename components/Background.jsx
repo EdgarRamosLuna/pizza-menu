6ae28a1e75d2d0ef6ea9c6ab3f41dc4acf14c3fc
Edgar Ramos Luna <edgarramoslun333@gmail.com>
2023-02-28 17:14:55 -0600

    Images Fixed

--- components/Background.jsx
+++ components/Background.jsx
@@ -5,7 +5,11 @@ const images = [
   { img: 0, w: 83, h: 89, l: 10, t: 10 },
   { img: 1, w: 82, h: 66, l: 80, t: 10 },
   { img: 2, w: 100, h: 100, l: 10, t: 90 },
+  { img: 2, w: 100, h: 100, l: 15, t: 35 },
   { img: 3, w: 103, h: 82, l: 80, t: 90 },
+  { img: 0, w: 83, h: 89, l: 90, t: 40 },
+  { img: 3, w: 103, h: 82, l: 5, t: 60 },
+  { img: 1, w: 82, h: 66, l: 85, t: 70 },
 ];
 
 const Background = ({ children }) => {
